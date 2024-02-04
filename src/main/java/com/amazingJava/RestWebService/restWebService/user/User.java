7e1446c771b9edb1f37cd6ced7cab00af2ca296e
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-05 05:16:12 +0600

    H2 database(in memory) connection with some initial insert values

--- src/main/java/com/amazingJava/RestWebService/restWebService/user/User.java
+++ src/main/java/com/amazingJava/RestWebService/restWebService/user/User.java
@@ -1,11 +1,17 @@
 package com.amazingJava.RestWebService.restWebService.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private  int id;
     @Size(min=2,message="Name should be atleast 2 characters")
     private  String name;
