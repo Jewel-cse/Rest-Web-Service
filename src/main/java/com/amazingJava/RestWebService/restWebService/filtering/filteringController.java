fbe87a3886e70718dfd9e878b2698c767b273688
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-05 01:12:39 +0600

    implement static filtering using: @JsonIgnoreProperties(field_name), which is class level

--- src/main/java/com/amazingJava/RestWebService/restWebService/filtering/filteringController.java
+++ src/main/java/com/amazingJava/RestWebService/restWebService/filtering/filteringController.java
@@ -1,2 +1,23 @@
-package com.amazingJava.RestWebService.restWebService.filtering;public class filteringController {
+package com.amazingJava.RestWebService.restWebService.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+
+public class filteringController {
+
+    @GetMapping("/filtering")
+    public SomeBean filtering(){
+        return new SomeBean("value1","value2","Value3");
+    }
+    @GetMapping("/filtering-list")
+    public List<SomeBean> filteringList(){
+        return Arrays.asList(new SomeBean("value1","value2","Value3"),
+                new SomeBean("value4","value5","Value6")) ;
+    }
 }
