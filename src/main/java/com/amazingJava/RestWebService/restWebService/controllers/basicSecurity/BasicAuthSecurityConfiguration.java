28aaccfcaaa0bfe4e8c0a5538bd45c2a53fc0800
Jewel-cse <md.jewel.cse@gmail.com>
2024-03-04 04:04:17 +0600

    storing user credentials in using jdbc, h2-console

--- src/main/java/com/amazingJava/RestWebService/restWebService/controllers/basicSecurity/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/amazingJava/RestWebService/restWebService/controllers/basicSecurity/BasicAuthSecurityConfiguration.java
@@ -2,13 +2,18 @@ package com.amazingJava.RestWebService.restWebService.controllers.basicSecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -29,8 +34,17 @@ public class BasicAuthSecurityConfiguration {
     }
     //no session, login form nai : cz form remove korechi, logout page nai
 
+
     @Bean
-    public UserDetailsService userDetailsService(){
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
         var user =  User.withUsername("jewel")
                 .password("{noop}1234")
                 .roles("USER")
@@ -38,9 +52,12 @@ public class BasicAuthSecurityConfiguration {
 
         var admin =  User.withUsername("admin")
                 .password("{noop}1234")
-                .roles("ADMIN")
+                .roles("ADMIN","USER")
                 .build();
-        return  new InMemoryUserDetailsManager(user,admin);
-    }
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
 
+        return  jdbcUserDetailsManager;
+    }
 }
